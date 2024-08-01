1a0f8861ba5a2d22476c25209c0431726dd35085
multifacs <kiwi0tails@gmail.com>
2024-08-01 06:47:03 +0300

    working premake5 project

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+-- premake5.lua
+workspace "ConanTutorial"
+configurations {"Debug", "Release"}
+architecture "x64"
+
+project "HelloWorld"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++20"
+targetdir "build/%{cfg.buildcfg}/bin"
+objdir "build/%{cfg.buildcfg}/obj"
+
+location "./src"
+files {"%{prj.location}/**.h", "%{prj.location}/**.cpp"}
+
+filter "configurations:Debug"
+defines {"DEBUG"}
+symbols "On"
+filter {}
+
+filter "configurations:Release"
+defines {"NDEBUG"}
+optimize "On"
+filter {}
